c11e86784edc51008d4f860e7a34e29c77cf5ef0
cq2021 <2972084238@qq.com>
2021-06-21 21:25:51 +0800

    生成后端springBoot框架，集成mybatis，mysql连接 设置pagehelper插件，mybatis generator 自动生成代码插件

---
+++ src/main/java/com/cq/lovehwh/LoveHwhApplication.java
@@ -0,0 +1,13 @@
+package com.cq.lovehwh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveHwhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoveHwhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cq/lovehwh/LoveHwhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cq.lovehwh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveHwhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
