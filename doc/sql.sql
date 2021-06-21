ece231e748641aaf91c58b5e5913ba913605cb86
cq2021 <2972084238@qq.com>
2021-06-21 22:17:23 +0800

    (第一次推送先进行这些操作) git pull git pull origin master git pull origin master --allow-unrelated-histories 删除一些不必要的文件和文件夹 编辑application.properties 生成数据库 增加日志打印文件 增加代码生成器文件

---
+++ doc/sql.sql
@@ -0,0 +1,15 @@
+drop table if exists `file`;
+drop table if exists `category`;
+create table `category`(
+    `id` bigint not null primary key comment 'ID',
+    `name` varchar(50) not null comment '分类名称',
+    `sort` int not null
+);
+
+create table `file`(
+    `id` bigint not null primary key comment '文件id',
+    `name` varchar(50) not null comment '文件名',
+    `file_path` varchar(50) not null comment '文件路径',
+    `create_time` date not null comment '创建时间',
+    `category_id` bigint references category(id)
+)
